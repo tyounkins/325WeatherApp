2573e45084261b43ec7df40b861d1e670c71b8d9
TaylorKern <taylor@kernmail.org>
2020-04-13 14:02:33 -0400

    Basic additions to Day, Precipitation, and Temperature classes

--- src/Day.java
+++ src/Day.java
@@ -1,4 +1,11 @@
 
-public class Day {
+public class Day
+{
+	private int date = new int;
+	private Precipitation dayPrecip = new Percipitation();
+	private Temperature dayTemp = new Temperature();
+	private String summaryDay = new String();
+	private String summaryCurrent = new String();
 
+	//Line 1312 in pastebin https://pastebin.com/ch7XjXhe
 }
--- src/Temperature.java
+++ src/Temperature.java
@@ -1,4 +1,10 @@
 
-public class Temperature {
-
+public class Temperature
+{
+	private double minTemp;
+	private double maxTemp;
+	private double lowTemp;
+	private double highTemp;
+	private double currentTemp;
+	private double apparentCurrTemp;
 }
